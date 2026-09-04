8c40a4165b1e13c70277e2a86a1530e684a9f2bd
yassminn8954 <ens-yasminscotti@ugv.edu.br>
2026-09-04 20:16:41 -0300

    aa

--- src/components/etapas.tsx
+++ src/components/etapas.tsx
@@ -15,7 +15,7 @@ export const etapas: Etapa[] = [
       },
       {
         tipo: "codigo",
-        conteudo: `kkkkkkkkkkkkkkkkkkkkkFROM node:20-alpine AS build
+        conteudo: `FROM node:20-alpine AS build
                     WORKDIR /app
                     COPY package*.json ./
                     RUN npm install
